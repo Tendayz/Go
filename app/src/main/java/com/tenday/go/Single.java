34b9f006b52e6acc17bfc006de3116c397ee78fb
Stepan <1800687@mail.ru>
2017-11-12 19:28:04 +0300

    Добавлен класс Single

--- app/src/main/java/com/tenday/go/Single.java
+++ app/src/main/java/com/tenday/go/Single.java
@@ -1,8 +1,19 @@
 package com.tenday.go;
 
-/**
- * Created by 18006 on 12.11.2017.
- */
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Single {
+public class Single extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
